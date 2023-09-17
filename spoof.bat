39ab162a70f459a1754ae5384127382c7e0f5fe9
xctrailerxdss <145224224+xctrailerxdss@users.noreply.github.com>
2023-09-17 03:34:13 +0300

    Update spoof.bat

--- spoof.bat
+++ spoof.bat
@@ -22,7 +22,7 @@ for /l %%i in (1,1,%guidLength%) do (
 
 :: Değişiklikleri kaydet
 set "newProductId=XCTRAILER!newNumberStr!"
-set "newMachineGuid=XCTRAILER!newNumberStr!!newGuid!"
+set "newMachineGuid=XCTRAILER!newNumberStr!"
 
 reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v "ProductId" /t REG_SZ /d "!newProductId!" /f
 reg add "HKLM\SOFTWARE\Microsoft\Cryptography" /v "MachineGuid" /t REG_SZ /d "!newMachineGuid!" /f
